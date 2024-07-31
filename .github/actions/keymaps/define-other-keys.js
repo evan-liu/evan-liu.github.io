f804b4772f3b42d9edbb7d4212dce68a3eb3b5f5
Evan Liu <evancoding@gmail.com>
2024-07-31 23:07:24 +1200

    ✏️ Fix typos in editor-keymaps

--- .github/actions/keymaps/define-other-keys.js
+++ .github/actions/keymaps/define-other-keys.js
@@ -32,19 +32,19 @@ export function defineOtherKeys() {
     {
       group: 'Window managment',
       keymap: [
-        { key: '⌃wr', desc: 'Move tab to right split', editors: [ws] },
         { key: '⌃wb', desc: 'Move tab to bottom split', editors: [ws] },
         { key: '⌃wm', desc: 'Move tab to opposite split', editors: [ws] },
+        { key: '⌃wr', desc: 'Move tab to right split', editors: [ws] },
         { key: '⌃wx', desc: 'Unsplit', editors: [ws] },
       ],
     },
     {
       group: 'Multiple cursors',
       keymap: [
+        { key: '⌘⌥⌃a', desc: 'Add cursor to all occurrences', editors: [ws] },
         { key: '⌘⌥⌃f', desc: 'Add cursor to next occurrence', editors: all },
         { key: '⌘⌥⌃s', desc: 'Skip occurrence', editors: [ws] },
         { key: '⌘⌥⌃x', desc: 'Remove occurrence', editors: [ws] },
-        { key: '⌘⌥⌃a', desc: 'Add cursor to all occurrences', editors: [ws] },
       ],
     },
   ]
