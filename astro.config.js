bfee7db4ffcefe176a1c9ae0c8dbef36e459db5f
Evan Liu <evancoding@gmail.com>
2024-11-09 18:10:57 +1300

    ➕ Install @astrojs/react

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://evanliu.dev',
   integrations: [
@@ -36,5 +38,6 @@ export default defineConfig({
       },
     }),
     tailwind({ applyBaseStyles: false }),
+    react(),
   ],
 })
