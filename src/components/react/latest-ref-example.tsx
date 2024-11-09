1d40f59e0c289c71fcb6c16ad64cea375f905d1c
Evan Liu <evancoding@gmail.com>
2024-11-09 18:11:22 +1300

    ✨ Add 'React Latest Ref Pattern'

---
+++ src/components/react/latest-ref-example.tsx
@@ -0,0 +1,56 @@
+import { type DependencyList, useEffect, useRef, useState } from 'react'
+
+export function useLatestRefEffect<const T extends { [key: string]: any }>(
+  effect: (latest: T) => ReturnType<typeof useEffect>,
+  deps: DependencyList,
+  latest: T,
+) {
+  let latestRef = useRef(latest)
+  let proxyRef = useRef<T>()
+  useEffect(() => {
+    latestRef.current = latest
+  })
+  useEffect(() => {
+    if (!proxyRef.current) {
+      proxyRef.current = new Proxy(latestRef, {
+        get: (target, p) => target.current[p as keyof T],
+      }) as unknown as T
+    }
+    return effect(proxyRef.current)
+  }, deps)
+}
+
+export function LatestRefExample() {
+  let [value, setValue] = useState('')
+  let [result, setResult] = useState<boolean | null>(null)
+  useLatestRefEffect(
+    (latest) => {
+      setResult(null)
+      check(value).then(
+        (valid) => latest.value == value && latest.setResult(valid),
+      )
+    },
+    [value],
+    { value, setResult },
+  )
+  return (
+    <div className="not-content mt-4 rounded-lg border border-sl-gray-5 p-4">
+      <label>
+        Value:
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="ml-2 rounded border border-sl-gray-5 px-2"
+        />
+      </label>
+      <span className="ml-4">
+        {result == null ? 'Checking...' : `Result: ${result}`}
+      </span>
+    </div>
+  )
+}
+
+async function check(handle: string) {
+  await new Promise((resolve) => setTimeout(resolve, 200 + Math.random() * 800))
+  return handle.length % 2 == 1
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/** Tailwind class constructing only, without merge(), use in app code. */
+export let cx = clsx
+
+/** Tailwind class constructing + merge(), used by ShadcnUI */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
